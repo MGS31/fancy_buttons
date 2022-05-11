53ebafdab98499d922a25c6cf36ad6a16901b301
Matt <mgstankov30@gmail.com>
2022-05-11 12:41:15 -0400

    update state of components to allow for changes on click

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
 
 function CounterButton(){
+  const [clickAmount, setClickAmount] = useState(0);
+
+  const handleClick = () => setClickAmount(clickAmount + 1);
+
   return(
-    <button className="CounterButton">
-        You clicked me X amount of times
-      </button>
+    <button onClick={handleClick} className="CounterButton">
+    You clicked me {clickAmount} times
+  </button>
   );
 };
 
