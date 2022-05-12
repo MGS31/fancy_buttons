d1124fe9ffc20f4e5d1a296ce23aa8be053f1d4f
Matt <mgstankov30@gmail.com>
2022-05-11 20:47:29 -0400

    finalize code to ensure the parent app.js handles the use state for the light switch button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
 import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
+import React, { useState } from 'react';
 
 
 function App() {
+  const [light, setLight] = useState('off');
+
+  const dark = (light === 'off') ? 'dark' : '';
+  const switchLight = () => setLight(light === 'on' ? 'off' : 'on');
+
   return (
-    <div className="App">
+    <div className={`App ${dark}`}>
       <h1><span>Fancy Buttons!</span></h1>
       <section>
       <AngryButton />
       <CounterButton />
-      <LightSwitchButton />
+      <LightSwitchButton light={light} setLight={switchLight}/>
       <TextRepeaterButton />
       </section>
     </div>
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 
-function LightSwitchButton(){
-  const [light, setLight] = useState('off');
-
-  const handleClick = () => setLight(light === 'on' ? 'off' : 'on');
+function LightSwitchButton(props){
+  const { light, setLight } = props;
 
   return(
-    <button onClick={handleClick} className="LightSwitchButton">
+    <button onClick={setLight} className="LightSwitchButton">
     {light === "on" && <span><i>💡</i> I'm on!</span>}
     {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
   </button>
